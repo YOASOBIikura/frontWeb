77143f34ad7d6558c4975e5819c9ba13b17d5e19
YOASOBIikura <793724308@qq.com>
2025-09-30 15:39:49 +0800

    update code

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,17 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VueI18nPlugin({
+        // 这里写你的语言包路径
+        include: fileURLToPath(new URL('./src/locales/*.json', import.meta.url)),
+    }),
     vueDevTools(),
   ],
   resolve: {
@@ -15,4 +20,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+    define: {
+        __VUE_I18N_FULL_INSTALL__: true,
+        __VUE_I18N_LEGACY_API__: true,  // 如果你用了 legacy API
+        __INTLIFY_PROD_DEVTOOLS__: false
+    }
 })
